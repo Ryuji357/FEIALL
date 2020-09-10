a673dd457ffdaccb39b263fd197b622aa406e2b5
Ryuji357 <gustavorsanomia@gmail.com>
2020-09-09 22:24:13 -0300

    Ultima simulação e nova versão do relatorio

--- NEA140 - SISTEMAS DE TELECOMUNICAÇÕES/calculo/pro.py
+++ NEA140 - SISTEMAS DE TELECOMUNICAÇÕES/calculo/pro.py
@@ -8,10 +8,21 @@ Created on Tue Sep  8 14:09:23 2020
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-from tkinter import *
 
 def ler_csv(caminho):
-    with open('eggs.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';', quotechar='"')
-        for row in spamreader:
-            yield {'teste1': row[0], 'teste2': row[1]}
+    with open(caminho, newline='') as csvfile:
+        spamreader = csv.reader(
+            csvfile,
+            delimiter=';',
+            quotechar='"'
+        )
+        result = csv.DictReader(spamreader)
+    return result
+
+if __name__ == '__main__':
+    pontos = ler_csv('teste.csv')
+    fig, ax = plt.subplots()
+    ax.plot(pontos['pos'], pontos['alt'])
+    ax.legend()
+
+    plt.show()
