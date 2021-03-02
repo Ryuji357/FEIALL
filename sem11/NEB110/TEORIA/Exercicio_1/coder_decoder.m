e05e3f7f90d3f5524a29bcfd75b0028f02e2d3eb
Ryuji357 <gustavorsanomia@gmail.com>
2021-03-02 20:03:02 -0300

    Adicionado x aleatorio

--- sem11/NEB110/TEORIA/Exercicio_1/coder_decoder.m
+++ sem11/NEB110/TEORIA/Exercicio_1/coder_decoder.m
@@ -5,7 +5,7 @@ m1 = [1 2 1 1 5 4 8 1 9];
 m2 = [1 2 2 1 8 1 6 7 9];
 
 n = 0:17; % Vetor de indice de tempo
-x = [1 3 4 6 -3 9 3 7 1 -5 2 6 9 9 4 5 -4 2]; % sequencia de amostras na entrada
+x = floor(rand(1, 18)*10).-5; % sequencia de amostras na entrada
 
 k1 = [m1 m2]; % Chave publica k1
 k2 = [m1 m1]; % Chave publica k2
