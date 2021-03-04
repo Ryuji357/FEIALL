533d72e439ee2fd046b731efc617e8797c852ccf
Gustavo Sanomia <gsanomia@planaudisp.local>
2021-03-04 15:40:54 -0300

    Atividade 2 NEB110

--- sem11/NEB110/LAB02/aula02_ex02.m
+++ sem11/NEB110/LAB02/aula02_ex02.m
@@ -3,9 +3,9 @@ clc
 clear
 
 f0 = 500; % Frequencia do sinal [Hz]
-tmax = 1e-2; % Tempo máximo de observação [s]
-fs = 33e3; % Frequencia de amostragem (2*f por Nyquist) [Hz]
-nbits=5; % Numero de bits
+tmax = 1; % Tempo máximo de observação [s]
+fs = 8e3; % Frequencia de amostragem (2*f por Nyquist) [Hz]
+nbits=16; % Numero de bits
 
 
 ts = 1/fs;
@@ -24,3 +24,17 @@ stem(t, xq, '.');
 hold off;
 
 audiowrite('xquant.wav', xq, fs);
+
+% Respostas 2.1:
+% 8KHz -> 8001 amostras
+% 4KHz -> 4001 amostras
+% 1KHz -> 1001 amostras
+% 900Hz -> 901 amostras
+% 500Hz -> 501 amostras
+% 100Hz -> 101 amostras
+
+% Respostas 2.2:
+% n = 16 -> 16*8.001= 128.016
+% n = 8 -> 8*8.001= 64.008
+% n = 4 -> 4*8.001= 32.004
+% n = 2 -> 2*8.001= 16.002
