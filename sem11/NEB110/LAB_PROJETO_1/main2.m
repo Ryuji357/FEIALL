4d7abd6248f4fb405b56587778d8f08167c9be01
Ryuji357 <gustavorsanomia@gmail.com>
2021-04-29 21:36:20 -0300

    LAB_PROJETO_1

--- sem11/NEB110/LAB_PROJETO_1/main2.m
+++ sem11/NEB110/LAB_PROJETO_1/main2.m
@@ -1,17 +1,22 @@
+quantidade = 5
+n = 1
+k = 1
+
 [eco, fs] = audioread('eco.wav');
+y = eco;
 
-atraso = 0.2*fs;
-y = zeros(1, length(eco));
+atraso = 0.2*fs
 
-for i=1:length(eco)
-  temp = i-atraso;
-  if (temp > 0)
-    y(i) = eco(i) - 0.7*y(temp);
-  endif
+h = [];
+for i=0:quantidade
+  h = [h zeros(1, atraso*i) 0.7*((-1)^i)];
 endfor
+
+y = conv(y, h);
 
 t1 = 0:1/length(eco):1;
 t2 = 0:1/length(y):1;
 plot(t1, [0; eco], t2, [0; y']);
+%plot(t1, [0; eco]);
 
 audiowrite('result.wav', y, fs);
