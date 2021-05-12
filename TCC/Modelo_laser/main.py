dc7141cfe2367bcca0eb09e5e9733f5df389512b
Gustavo Sanomia <gsanomia@planaudisp.local>
2021-05-11 21:48:29 -0300

    atualização de sincronia

--- TCC/Modelo_laser/main.py
+++ TCC/Modelo_laser/main.py
@@ -2,17 +2,18 @@ import math
 
 # Parametros do laser
 w = 810 # Comprimento de onda [nm]
-i = 1 # Corrente [A]
+eficiencia = 0.4 # Eficiencia (nominal)
+p_l = 40 # Potencia do laser (nominal) [w]
 
 # Parametros do sistema
-d = 5000 # Distancia [m]
+d = 100 # Distancia [m]
 
 # Parametros da atmosfera
 capa = 10 # Visibilidade (6km<capa<50km para ar limpo) [km]
 ro = 1.3 # Tamanho da distribuição de particulas (1.3 para ar limpo)
 
 # Constantes dos calculos (não mexa)
-c = 1 # Velocidade da luz [m/s]
+c = 299792458 # Velocidade da luz [m/s]
 k = 1.38064852e-23 # Constante de Boltzmann
 h = 6.62606957e-34 # Constante de Planck
 
@@ -21,6 +22,6 @@ qui = 550 # [nm]
 
 # Eficiencia da tranmissão
 alfa = (sigma/capa)*((w/qui)**(-ro))
-n_tra = math.exp(alfa*d/1000)
+n_tra = math.exp(-alfa*d/1000)
 
 print(n_tra)
