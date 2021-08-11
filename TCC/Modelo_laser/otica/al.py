38516294b32e3825a1b40439c534a2d28b35e9d9
Gustavo Sanomia <gsanomia@planaudisp.local>
2021-08-10 21:42:23 -0300

    Modelo laser

--- TCC/Modelo_laser/otica/al.py
+++ TCC/Modelo_laser/otica/al.py
@@ -1,25 +1,110 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug  5 23:48:57 2021
+Created on Fri Aug  6 23:46:54 2021
 
 @author: Ryuji
 """
 
-class vetor:
-    x = 0
-    y = 0
+class ponto:
+    x = float()
+    y = float()
 
     def __init__(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __str__(self):
-        return 'Vetor [{} {}]'.format(self.x, self.y)
+        return 'Ponto: [{0} {1}]'.format(self.x, self.y)
 
+    def __repr__(self):
+        return 'ponto(x={0}, y={1})'.format(self.x, self.y)
+
+    # Binary Operators
     def __add__(self, other):
-        if type(other) is vetor:
-            x = self.x + other.x
-            y = self.y + other.y
-            return vetor(x, y)
+        if isinstance(other, vetor):
+            return ponto(self.x + other.x, self.y + other.y)
         else:
-            raise ValueError('Valores não compativeis.')
+            raise ValueError('Operação inválida.')
+
+    def __sub__(self, other):
+        if isinstance(other, vetor):
+            #print('Operação não recomendada, mude o sentido do vetor antes.')
+            return vetor(self.x - other.x, self.y - other.y)
+        elif isinstance(other, ponto):
+            return vetor(self.x - other.x, self.y - other.y)
+        else:
+            raise ValueError('Operação inválida.')
+
+class vetor:
+    x = float()
+    y = float()
+
+    def __init__(self, x = 0, y = 0):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __str__(self):
+        return 'Vetor: [{0} {1}]'.format(self.x, self.y)
+
+    def __repr__(self):
+        return 'vetor(x={0}, y={1})'.format(self.x, self.y)
+
+    # Binary Operators
+    def __add__(self, other):
+        if isinstance(other, vetor):
+            return vetor(self.x + other.x, self.y + other.y)
+        elif isinstance(other, ponto):
+            return ponto(self.x + other.x, self.y + other.y)
+        else:
+            raise ValueError('Operação inválida.')
+
+    def __sub__(self, other):
+        if isinstance(other, vetor):
+            #print('Operação não recomendada, mude o sentido do vetor antes.')
+            return vetor(x = self.x - other.x, y = self.y - other.y)
+        else:
+            raise ValueError('Operação inválida.')
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return vetor(x = self.x*other, y = self.y*other)
+            #a = abs(self)
+            #h = a * other
+            #x = h*(self.x/a)
+            #y = h*(self.y/a)
+            #return vetor(x = x, y = y)
+        else:
+            raise ValueError('Operação inválida.')
+
+    def __rmul__(self, other):
+        if isinstance(other, (int, float)):
+            return vetor(x = self.x*other, y = self.y*other)
+            #a = abs(self)
+            #h = a * other
+            #x = h*(self.x/a)
+            #y = h*(self.y/a)
+            #return vetor(x = x, y = y)
+        else:
+            raise ValueError('Operação inválida.')
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return vetor(x = self.x/other, y = self.y/other)
+        else:
+            raise ValueError('Operação inválida.')
+
+    # Unary Operators
+    def __neg__(self):
+        return vetor(-self.x, -self.y)
+
+    def __abs__(self):
+        return float(((self.x)**2 + (self.y)**2)**0.5)
+
+    # Outras operações
+    def versor(self):
+        return self*(1/abs(self))
+
+# Pontos e vetores notaveis
+O = ponto(0, 0)
+i = vetor(1, 0)
+j = vetor(0, 1)
--- TCC/Modelo_laser/otica/main.py
+++ TCC/Modelo_laser/otica/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 13 14:44:03 2021
+Created on Fri Aug  6 23:45:03 2021
 
 @author: Ryuji
 """
