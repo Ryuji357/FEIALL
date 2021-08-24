c9b147d79728b2a0bdeda3c5a0bddf16f8321f63
Ryuji357 <gustavorsanomia@gmail.com>
2021-08-24 17:24:06 -0300

    LAB 1 finish

--- sem12/NEC130/LAB01/atividade.m
+++ sem12/NEC130/LAB01/atividade.m
@@ -17,42 +17,81 @@ R = X(:,:,1); %separa o canal R na matriz R
 G = X(:,:,2); %separa o canal G na matriz G
 B = X(:,:,3); %separa o canal B na matriz B
 
-subplot(1,3,1) %visualiza as três matrizes no mapa de cor definido
-image(R)
-title('canal R')
+figure(1, 'name', 'RGB', 'numbertitle', 'off');
+
+subplot(2,2,1);
+imshow(X);
+title('Original');
 axis off;
+axis('equal');
 
-subplot(1,3,2)
-image(G)
-title('canal G')
+subplot(2,2,2);
+image(R);
+title('canal R');
 axis off;
+axis('equal');
 
-subplot(1,3,3)
-image(B)
-title('canal B')
+subplot(2,2,3);
+image(G);
+title('canal G');
 axis off;
+axis('equal');
 
-%% A função colormap gera uma matriz de com cores, ao gerar uma figure relaciona
-%com os numeros inteiros. Altera-lo gera esquemas de cores diferentes.
-
-Y = 0.587*G + 0.299*R + 0.114*B; %calcula o valor da luminância em função de R,G,B
-Cb = 128 + 0.564*(B-Y);%calcula o valor de Pb em função de R,G,B
-Cr = 128 + 0.713*(R-Y);%calcula o valor de Pr em função de R,G,B
-figure %abre uma nova imagem
-
-colormap(gray(256));
-subplot(1,3,1) %visualiza as três matrizes no mapa de cor definido
-image(Y)
-title('canal de luminância')
+subplot(2,2,4);
+image(B);
+title('canal B');
 axis off;
+axis('equal');
 
-subplot(1,3,2)
-image(Cb)
-title('canal de Cb')
+%% YCbCr
+
+Y = 0.587*G + 0.299*R + 0.114*B; % Calcula o valor da luminância em função de R,G,B
+Cb = 128 + 0.564*(B-Y);% Calcula o valor de Cb em função de R,G,B
+Cr = 128 + 0.713*(R-Y);% Calcula o valor de Cr em função de R,G,B
+
+figure(2, 'name', 'YCbCr', 'numbertitle', 'off');
+
+% Set colormap
+%colormap(hot(256));
+colormap(jet(256));
+
+a = subplot(2,2,1);
+imshow(X);
+title('Original');
 axis off;
+axis('equal');
 
-subplot(1,3,3)
-image(Cr)
-title('canal de Cr')
+b = subplot(2,2,2);
+imagesc(Y);
+title('canal de luminância');
 axis off;
+axis('equal');
 
+c = subplot(2,2,3);
+image(Cb);
+title('canal de Cb');
+axis off;
+axis('equal');
+
+d = subplot(2,2,4);
+image(Cr);
+title('canal de Cr');
+axis off;
+axis('equal');
+
+linkaxes([a, b, c, d]);
+
+%% Respostas
+
+% Pergunta: A função colormap gera uma matriz de com cores, ao gerar uma figure
+%relaciona com os numeros inteiros. Altera-lo gera esquemas de cores diferentes,
+%por exemplo: em uma imagem RGB, podemos relacionar os valores de R (red) ao
+%colormap, então podemos ver a intensidade da componente R na imagem em um
+%padrão de cinza ou qualquer outro esquema de cores disponipinizado para
+%facilitar a vizualização.
+
+% Conclusões: O Octave é uma ferramenta poderosa, que pode tambem ser utilizado
+%para analise de imagens, nota-se que:
+%A função colormap é definida para toda a figure, assim, todos os subplots terão
+%que utilizar o mesmo colormap, porém, ao utilizar a função 'imshow', o colormap
+%se torna irrelevante.
